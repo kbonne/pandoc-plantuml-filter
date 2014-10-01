7cee14067edfbe2a4004e49427efe462197bf3e4
Kurt Bonne <kurt.bonne@gmail.com>
2014-10-01 21:03:17 +0200

    Initial commit. Uses some hard-coded options, but filter is usable.

---
+++ README.md
@@ -0,0 +1,55 @@
+# Pandoc PlantUML Filter
+
+Replaces `CodeBlocks` with `plantuml` class with PlantUML generated UML diagram
+
+**This is a work in progress.**
+
+## Requirements
+
+- `plantuml` in `PATH` which launches PlantUML jar (see `scripts` folder)
+- `pandoc`
+
+## Usage
+
+e.g. Generating .pdf from this file
+```
+pandoc README.md --variable=geometry:a4paper,margin=2cm --variable=fontsize:11pt -o README.pdf --filter ./dist/build/pandoc-plantuml-filter/pandoc-plantuml-filter
+```
+
+## Tests
+
+### Javascript
+
+Should be skipped
+
+```javascript
+var s = "JavaScript syntax highlighting";
+alert(s);
+```
+
+### PlantUML
+
+Should be processed!
+
+```plantuml
+@startuml
+Alice -> Bob: Authentication Request
+Bob --> Alice: Authentication Response
+
+Alice -> Bob: Another authentication Request
+Alice <-- Bob: another authentication Response
+@enduml
+```
+
+This one as well
+
+```{.plantuml include="README.md"}
+@startuml
+class Car
+
+Driver - Car : drives >
+Car *- Wheel : have 4 >
+Car -- Person : < owns
+
+@enduml
+```
---
+++ src/HandlePlantUMLBlock.hs
@@ -0,0 +1,41 @@
+import Text.Pandoc.JSON
+import Data.ByteString.Lazy (hGetContents, hPut)
+import Data.ByteString.Lazy.UTF8 (fromString)
+import Data.Digest.Pure.SHA (sha1, showDigest)
+import System.IO (hClose, hPutStr, IOMode(..), openBinaryFile)
+import System.Process
+
+processBlocks :: Block -> IO Block
+processBlocks b =
+  case b of
+    CodeBlock (_ , ["plantuml"], _) content -> plantUMLToImg content
+    _ -> return b
+
+plantUMLToImg :: String -> IO Block
+plantUMLToImg content =  do
+  path <- renderImage content
+  return $ Para [Image [] (path, "")]
+
+renderImage :: String -> IO String
+renderImage content = do
+  let path = uniqueName content ++ ".eps"
+  (Just hIn, Just hOut, _, _) <-
+    createProcess (proc "plantuml" ["-pipe", "-teps"]){ std_in = CreatePipe,
+                                                        std_out = CreatePipe }
+  hPutStr hIn content
+  hClose hIn
+
+  hFile <- openBinaryFile path WriteMode
+  img <- hGetContents hOut
+  hPut hFile img
+
+  hClose hFile
+  hClose hOut
+
+  return path
+
+uniqueName :: String -> String
+uniqueName = showDigest . sha1 . fromString
+
+main :: IO ()
+main = toJSONFilter processBlocks
